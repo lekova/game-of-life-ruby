c6d1f0257392f7aa8902daa9fd3334c00daa3a18
lekova <lina.lekova@gmail.com>
2015-07-20 13:52:07 -0400

    removes all references to cell.rb

--- board.rb
+++ board.rb
@@ -1,5 +1,4 @@
 require 'pry'
-require './cell.rb'
 
 class Board
   attr_reader :grid
@@ -14,55 +13,47 @@ class Board
   def init_board
     for i in (0..@rows - 1)
       for j in (0..@cols - 1)
-        @grid[i][j] = Cell.new false
-        @grid[i][j].set_random
+        @grid[i][j] = set_random
       end
     end
   end
 
-  def print_board
-    result = Array.new(@rows) { Array.new(@cols) }
-    for i in (0..@rows - 1)
-      for j in (0..@cols - 1)
-        result[i][j] = 1 if @grid[i][j].is_alive
-        result[i][j] = 0 if !@grid[i][j].is_alive
-      end
-      print "#{result[i]} \n"
-    end
+  def set_random
+    Random.rand(2) == 1 ? true : false
   end
 
   def get_neighbors x, y
     num_of_neighbors = 0
 
-    if x - 1 >= 0 && y - 1 >= 0 && y < @cols && @grid[x - 1][y - 1].is_alive
+    if x - 1 >= 0 && y - 1 >= 0 && y < @cols && @grid[x - 1][y - 1]
       num_of_neighbors += 1
     end
 
-    if x - 1 >= 0 && y >= 0 && @grid[x - 1][y].is_alive
+    if x - 1 >= 0 && y >= 0 && @grid[x - 1][y]
       num_of_neighbors += 1
     end
 
-    if x - 1 >= 0 && y + 1 < @cols && @grid[x - 1][y + 1].is_alive
+    if x - 1 >= 0 && y + 1 < @cols && @grid[x - 1][y + 1]
       num_of_neighbors += 1
     end
 
-    if y - 1 >= 0 && @grid[x][y - 1].is_alive
+    if y - 1 >= 0 && @grid[x][y - 1]
       num_of_neighbors += 1
     end
 
-    if y + 1 < @cols && @grid[x][y + 1].is_alive
+    if y + 1 < @cols && @grid[x][y + 1]
       num_of_neighbors += 1
     end
 
-    if x + 1 < @rows && y - 1 >= 0 && @grid[x + 1][y - 1].is_alive
+    if x + 1 < @rows && y - 1 >= 0 && @grid[x + 1][y - 1]
       num_of_neighbors += 1
     end
 
-    if x + 1 < @rows && @grid[x + 1][y].is_alive
+    if x + 1 < @rows && @grid[x + 1][y]
       num_of_neighbors += 1
     end
 
-    if x + 1 < @rows && y + 1 < @cols && @grid[x + 1][y + 1].is_alive
+    if x + 1 < @rows && y + 1 < @cols && @grid[x + 1][y + 1]
       num_of_neighbors += 1
     end
 
@@ -75,13 +66,13 @@ class Board
     for i in (0..@rows-1)
       for j in (0..@cols-1)
         neighbors = get_neighbors i, j
-        # puts "neighbors for[#{i}][#{j}]: #{neighbors}"
-        if @grid[i][j].is_alive && ( neighbors < 2 || neighbors > 3 )
-          new_grid[i][j] = Cell.new(false)
-        elsif @grid[i][j].is_alive && (neighbors == 2 || neighbors == 3)
-          new_grid[i][j] = Cell.new(true)
-        elsif !@grid[i][j].is_alive && neighbors == 3
-          new_grid[i][j] = Cell.new(true)
+
+        if @grid[i][j] && ( neighbors < 2 || neighbors > 3 )
+          new_grid[i][j] = false
+        elsif @grid[i][j] && (neighbors == 2 || neighbors == 3)
+          new_grid[i][j] = true
+        elsif !@grid[i][j] && neighbors == 3
+          new_grid[i][j] = true
         else
           new_grid[i][j] = @grid[i][j]
         end
@@ -96,7 +87,7 @@ class Board
     canvas = Drawille::Canvas.new
 
     each_grid do |x, y|
-      canvas.set(x, y) if x < @rows && y < @cols && @grid[x][y].is_alive == true
+      canvas.set(x, y) if x < @rows && y < @cols && @grid[x][y] == true
     end
     canvas
   end
@@ -104,8 +95,8 @@ class Board
   private
 
   def each_grid
-    (0...@rows).each do |y|
-      (0...@cols).each do |x|
+    (0...@rows - 1).each do |y|
+      (0...@cols - 1).each do |x|
         yield x, y
       end
     end
