cce44b359370a95e72a8192b42f5c8e0bd372a7f
lekova <lina.lekova@gmail.com>
2015-07-20 13:04:44 -0400

    removes puts and comment from game.rb

--- game.rb
+++ game.rb
@@ -4,11 +4,7 @@ require './board.rb'
 
 Curses::init_screen()
 
-puts "Curses.lines: #{Curses.lines}"
-puts "Cursed.cols: #{Curses.cols * 2 - 2}"
-# board   = Board.new 4, 4
 board = Board.new Curses.lines * 4, Curses.cols * 2 - 2
-
 flipbook = Drawille::FlipBook.new
 
 flipbook.play do
